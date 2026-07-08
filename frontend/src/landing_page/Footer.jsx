3ea88e307654773fe26909f4f918fe40f402ffeb
Ravikant singh <thakurravikantsingh550@gmail.com>
2026-07-08 19:20:13 +0530

    created navbar and footer

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -1,9 +1,163 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return ( 
-        <h1>footer</h1>
-     );
+  return (
+    <div className="container-fluid border-top bg-light">
+        <div className="container">
+      <div className="row mt-5">
+        <div className="col">
+          <img
+            src="media/images/logo.svg"
+            alt="logo"
+            style={{ width: "45%" }}
+          />
+          <p>&copy; 2010 - 2026, Zerodha Broking Ltd. All rights reserved.</p>
+          <div className="row"></div>
+        </div>
+        <div className="col">
+          <p>Account</p>
+          <a href="" style={{textDecoration:"none"}} className="text-muted mb-4">Open demat account </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4">Minor demat account </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4">NRI demat account </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted mb-4">HUF demat account </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4">Commodity </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted mb-4">Dematerialisation </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4">Fund transfer</a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4">MTF </a> <br></br>
+        </div>
+        <div className="col" >
+          <p>Support</p>
+          <a href="" style={{textDecoration:"none"}} className="text-muted">Contact us </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted">Support portal </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted">How to file a complaint? </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted"> Status of your complaints </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted"> Bulletin </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted"> Circular </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted"> Z-Connect blog </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted"> Downloads </a> <br></br>
+        </div>
+        <div className="col">
+          <p>Company</p>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> About </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Philosophy </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Press & media </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Careers </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Zerodha Cares (CSR) </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Zerodha.tech </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Open source </a>
+          <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Referral program </a>
+          <br></br>
+        </div>
+        <div className="col">
+          <p>Quick links</p>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Upcoming IPOs </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Brokerage charges </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Market holidays </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Economic calendar </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Calculators </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4">Markets </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Sectors </a> <br></br>
+          <a href="" style={{textDecoration:"none"}} className="text-muted  mb-4"> Gift Nifty </a> <br></br>
+        </div>
+        <div className="mt-5  text-muted" style={{fontSize:"12px"}}>
+        <p >
+          Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI
+          Registration no.: INZ000031633 CDSL/NSDL: Depository services through
+          Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019
+          Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars
+          Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru -
+          560078, Karnataka, India. For any complaints pertaining to securities
+          broking please write to complaints@zerodha.com, for DP related to
+          dp@zerodha.com. Please ensure you carefully read the Risk Disclosure
+          Document as prescribed by SEBI | ICF{" "}
+        </p>
+
+        <p>
+          Procedure to file a complaint on SEBI SCORES: Register on SCORES
+          portal. Mandatory details for filing complaints on SCORES: Name, PAN,
+          Address, Mobile Number, E-mail ID. Benefits: Effective Communication,
+          Speedy redressal of the grievances{" "}
+        </p>
+
+        <p>
+          {" "}
+          Smart Online Dispute Resolution | Grievances Redressal Mechanism{" "}
+        </p>
+
+        <p>
+          {" "}
+          Investments in securities market are subject to market risks; read all
+          the related documents carefully before investing.
+        </p>
+
+        <p>
+          Attention investors: 1) Stock brokers can accept securities as margins
+          from clients only by way of pledge in the depository system w.e.f
+          September 01, 2020. 2) Update your e-mail and phone number with your
+          stock broker / depository participant and receive OTP directly from
+          depository on your e-mail and/or mobile number to create pledge. 3)
+          Check your securities / MF / bonds in the consolidated account
+          statement issued by NSDL/CDSL every month.
+        </p>
+
+        <p>
+          {" "}
+          India's largest broker based on networth as per NSE. NSE broker
+          factsheet
+        </p>
+
+        <p>
+          {" "}
+          "Prevent unauthorised transactions in your account. Update your mobile
+          numbers/email IDs with your stock brokers/depository participants.
+          Receive information of your transactions directly from
+          Exchange/Depositories on your mobile/email at the end of the day.
+          Issued in the interest of investors. KYC is one time exercise while
+          dealing in securities markets - once KYC is done through a SEBI
+          registered intermediary (broker, DP, Mutual Fund etc.), you need not
+          undergo the same process again when you approach another
+          intermediary." Dear Investor, if you are subscribing to an IPO, there
+          is no need to issue a cheque. Please write the Bank account number and
+          sign the IPO application form to authorize your bank to make payment
+          in case of allotment. In case of non allotment the funds will remain
+          in your bank account. As a business we don't give stock tips, and have
+          not authorized anyone to trade on behalf of others. If you find anyone
+          claiming to be part of Zerodha and offering such services, please
+          create a ticket here.{" "}
+        </p>
+
+        <p>
+          {" "}
+          *Customers availing insurance advisory services offered by Ditto
+          (Tacterial Consulting Private Limited | IRDAI Registered Corporate
+          Agent (Composite) License No CA0738) will not have access to the
+          exchange investor grievance redressal forum, SEBI SCORES/ODR, or
+          arbitration mechanism for such products.{" "}
+        </p>
+
+        <p>
+          {" "}
+          Fixed deposit products offered on this platform are third-party
+          products (TPP) and are not Exchange traded products. These are offered
+          through Blostem Fintech Private Limited. Zerodha Broking Limited (SEBI
+          Registration No.: INZ000031633) is acting solely as a distributor for
+          these products. Any disputes arising with respect to such distribution
+          activity will not have access to SEBI SCORES/ODR, Exchange Investor
+          Grievance Redressal Forum, or Arbitration mechanism. Fixed deposits
+          are regulated by the Reserve Bank of India (RBI).{" "}
+        </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
 
 export default Footer;
