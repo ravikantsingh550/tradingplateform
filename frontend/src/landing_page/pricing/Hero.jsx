2c52e3005ab38654530f584643f9b6b596eedb7d
Ravikant singh <thakurravikantsingh550@gmail.com>
2026-07-09 23:20:20 +0530

    created pricing page

--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 function Hero() {
     return ( 
-        <h1>hero</h1>
+        <div className="container">
+            <div className="row text-center mt-5 ">
+                <h1>Charges</h1>
+                <p className='fs-4 text-muted mb-5'>List of all charges and taxes</p>
+
+                <div className="col-4 mt-5 p-5 ">
+                    <img src='media/images/pricing-eq.svg' style={{width:"70%"}} className='mb-3'/>
+                    <h1 className='fs-3'>Free equity delivery</h1>
+                    <p className='text-muted mt-3'>All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+                </div>
+                <div className="col-4 mt-5 p-5">
+                    <img src='media/images/intradayTrades.svg' style={{width:"70%"}} className='mb-3'/>
+                    <h1 className='fs-3'>Intraday and F&O trades</h1>
+                    <p className='text-muted mt-3'>Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+                </div>
+                <div className="col-4 mt-5 p-5">
+                    <img src='media/images/pricing-eq.svg' style={{width:"70%"}} className='mb-3'/>
+                    <h1 className='fs-3'>Free direct MF</h1>
+                    <p className='text-muted mt-3'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+                </div>
+            </div>
+        </div>
      );
 }
 
--- frontend/src/landing_page/pricing/PricingPage.jsx
+++ frontend/src/landing_page/pricing/PricingPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Hero from './Hero';
 import Brokerage from './Brokerage';
+import OpenAccount from '../OpenAccount';
 
 function PricingPage() {
     return (
-        <h1>
-            <Hero/>
+        <>
+        <Hero/>
+            <OpenAccount/>
             <Brokerage/>
-        </h1>
+        </>
       );
 }
 
