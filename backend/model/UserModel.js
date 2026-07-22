d3ee46032173f777ebf0e14e007563d047cebab4
Ravikant singh <thakurravikantsingh550@gmail.com>
2026-07-22 22:39:04 +0530

    create user schema and authcontroller for that

---
+++ backend/model/UserModel.js
@@ -0,0 +1,9 @@
+const {model}  = require("mongoose");
+const bcrypt = require("bcryptjs");
+const {UserSchema} = require("../schemas/UserSchema");
+
+UserSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+module.exports = mongoose.model("User", UserSchema);
